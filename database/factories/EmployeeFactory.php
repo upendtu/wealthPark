f6650f3037bf0109719c6a644ff4f6d0c37d37b0
upendtu <upendtu@gmail.com>
2019-08-03 10:09:56 +0900

    add model ,db,fake data

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Employee;
+use Faker\Generator as Faker;
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_08_02_160609_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('e_firstName');
+            $table->string('e_lastName');
+            $table->date('e_dob');
+            $table->text('e_address');
+            $table->string('e_bossName');
+            $table->integer('e_salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
